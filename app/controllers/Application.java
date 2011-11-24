9de6eb1c4402ae63e61ad08252d4029828618610
Daniel Wildt <dwildt@gmail.com>
2011-11-24 07:36:39 -0200

    adding more KPIs

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,22 +20,4 @@ public class Application extends Controller {
     public static void about() {
         render();
     }
-
-    public static void semana() {
-    	KeyProcessIndicator kpi = 
-    		KeyProcessIndicator.find("bySlug", "semana").first();
-
-    	List<KPIData> kpiDataItems = 
-    		KPIData.find("byKpi", kpi).fetch();
-    	
-    	//KeyProcessIndicator kpi = 
-    //			new KeyProcessIndicator("semana",100,true);
-    	//    List<KPIData> listaPontosRealizados = new ArrayList<KPIData>();
-    	//    listaPontosRealizados.add(new KPIData(kpi,21,60,23));
-    	//    listaPontosRealizados.add(new KPIData(kpi,22,60,42));    	    
-    	//    listaPontosRealizados.add(new KPIData(kpi,23,60,86));
-    	
-        render(kpi, kpiDataItems);
-    }
-
 }
--- app/controllers/KPIViewer.java
+++ app/controllers/KPIViewer.java
@@ -10,20 +10,21 @@ import models.*;
 public class KPIViewer extends Controller {
 
     public static void show(String slug) {
-    	KeyProcessIndicator kpi = 
-    		KeyProcessIndicator.find("bySlug", slug).first();
+    		KeyProcessIndicator kpi = 
+    			KeyProcessIndicator.find("bySlug", slug).first();
 
-    	List<KPIData> kpiDataItems = 
-    		KPIData.find("byKpi", kpi).fetch();
-    	
-    	//KeyProcessIndicator kpi = 
-    //			new KeyProcessIndicator("semana",100,true);
-    	//    List<KPIData> listaPontosRealizados = new ArrayList<KPIData>();
-    	//    listaPontosRealizados.add(new KPIData(kpi,21,60,23));
-    	//    listaPontosRealizados.add(new KPIData(kpi,22,60,42));    	    
-    	//    listaPontosRealizados.add(new KPIData(kpi,23,60,86));
+    		List<KPIData> kpiDataItems = 
+    			KPIData.find("byKpi", kpi).fetch();
     	
         render(kpi, kpiDataItems);
+    }
+    
+    public static void all(){
+		List<KeyProcessIndicator> kpis = 
+			KeyProcessIndicator.all().fetch();
+
+        render(kpis);
+    	
     }
 
 }
